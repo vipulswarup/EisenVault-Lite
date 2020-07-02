385298da57f23f3bd617066115f8dbf670b82a2b
ShayaneB <shayane.basu@eisenvault.com>
2020-07-02 17:09:26 +0530

    Login

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,14 +1,15 @@
 import React, { Fragment } from "react";
 import './styles.scss';
+import { HashRouter as Router } from "react-router-dom";
 
 import Routings from './Routings';
 
 const App = () => {
   return (
     <Fragment>
-      <div>
+      <Router>
         <Routings />
-      </div>
+      </Router>
     </Fragment>
   )
 }
--- src/Containers/Routings.js
+++ src/Containers/Routings.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import React, { Fragment } from "react";
+import { Route, withRouter } from "react-router-dom";
 // import LoginRoutings from './LoginRoutings';
 import LoginPage from "../Components/Login/Login"
 import NavigationItems from "../Components/Navigation/NavigationItems/NavigationItems";
-import MobileMenu from "../Components/MobileMenu/MobileMenu"
+import MobileMenu from "../Components/MobileMenu/MobileMenu";
 
 import MyUploads from "../Components/MyUploads/MyUploads";
 import Dashboard from "../Components/Dashboard/Dashboard";
@@ -15,31 +15,56 @@ import ManageShares from "../Components/ManageShares/ManageShares";
 
 import './styles.scss';
 
-const Routings = () => {
-  return  <Router>
 
-    {/* <Switch> */}
+// const Routings = withRouter(({location}) => {
+//   return  <Router>
 
-    <Route exact path="/" component={LoginPage} />
+//     {/* <Switch> */}
 
-    <div>
-    <MobileMenu />
-    </div>
+//     <Route exact path="/" component={LoginPage} />
+//     <Route>
+//     {location.pathname !== '/' && <NavigationItems />} 
+//     </Route>
     
-    <div className="main_body">
+//     <div>
+//     <MobileMenu />
+//     </div>
+    
+//     <div className="main_body">
 
-        <NavigationItems />
+//         {/* <NavigationItems /> */}
+//         <Route path="/dashboard" exact component ={Dashboard} />
+//         <Route path="/documentsList" component={documentsList} />
+//         <Route path="/myUploads" component={MyUploads} />
+
+//         <Route path="/trashDisplay" component={TrashDisplayFiles} />
+
+//         <Route path="/manageShares" component={ManageShares} />
+
+//     </div>
+//    {/* </Switch> */}
+//   </Router>;
+// })
+
+const Routings = withRouter (({ location }) => {
+  return(
+    <Fragment>
+        <Route exact path="/" component={LoginPage} />
+      <div>
+        {location.pathname !== '/' && <MobileMenu />} 
+      </div>
+      <div className="main_body">
+        {location.pathname !== '/' && <NavigationItems /> }
+
         <Route path="/dashboard" exact component ={Dashboard} />
         <Route path="/documentsList" component={documentsList} />
         <Route path="/myUploads" component={MyUploads} />
-
         <Route path="/trashDisplay" component={TrashDisplayFiles} />
-
         <Route path="/manageShares" component={ManageShares} />
-
-    </div>
-   {/* </Switch> */}
-  </Router>;
+      </div>
+    </Fragment>
+  )
 }
+)
 
 export default Routings;
