1b73fcd4cb499155243b43c73c32aa3b1c4d24c1
ShayaneB <shayane.basu@eisenvault.com>
2020-07-03 14:54:26 +0530

    Login Updated

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,22 +2,26 @@ import React, { Fragment, useState } from 'react';
 import axios from 'axios';
 import ModalForgetPswd from '../UI/Modal/ModalForgetPswd';
 import useModal from '../UI/Modal/useModal';
-import { setUserSession } from '../../Utils/Common';
+import { setUserLocal } from '../../Utils/Common';
 import './LoginPage.scss';
 
 const LoginPage = (props) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  // const userName = useState ('');
-  // const password = useState ('');
+  const userName = useFormInput ('');
+  const password = useFormInput ('');
 
   const handleLogin = () => {
+
     setError(null);
     setLoading(true);
+ //{ userId: 'admin', password: 'Systest@987'}
+
     axios.post('https://systest.eisenvault.net/alfresco/api/-default-/public/authentication/versions/1/tickets', 
-    { userId: "admin", password: "Systest@987" }).then(response => {
+    { userId: userName.value, password: password.value}).then(response => {
       setLoading(false);
-      setUserSession(response.data.token, response.data.user);
+      setUserLocal(response.data.id, response.data.userId);
+      // console.log(getToken, getUser, setUserLocal);
       props.history.push('/dashboard');
     }).catch(error => {
       setLoading(false);
@@ -37,14 +41,21 @@ const LoginPage = (props) => {
 
         <div className="login-box">
             <div className="login-details">
-                <input type="text" id="user-name" placeholder="User Name" required/>
+                <input type="text" {...userName} 
+                id="user-name" placeholder="User Name" required/>
                   <br />
-                <input type="password" placeholder="Password" id="pswd" required/>
+                <input type="password" {...password}
+                placeholder="Password" id="pswd" required/>
             </div>
 
             <div id="btns_new">
-                <button id="btn01" type="button" onClick={handleLogin}>
-                  Login</button>
+                {/* <button id="btn01" type="button" onClick={handleLogin}>
+                  Login</button> */}
+                {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}
+                <br />
+                <button id="btn01" type="button" value={loading ? 'Loading...' : 'Login'} 
+                onClick={handleLogin} disabled={loading}>
+                Login</button>
                 <br />
                                 
                   <button id="btn02" type="button" onClick={toggle1}>
@@ -58,16 +69,16 @@ const LoginPage = (props) => {
   
 }
 
-// const useFormInput = initialValue => {
-//   const [value, setValue] = useState(initialValue);
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
  
-//   const handleChange = e => {
-//     setValue(e.target.value);
-//   }
-//   return {
-//     value,
-//     onChange: handleChange
-//   }
-// }
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
+}
 
 export default LoginPage;
